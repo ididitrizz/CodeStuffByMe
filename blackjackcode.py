87415c3b561ba9b1053ab020306a0c920c1b1ac9
I.Did.It.Rizz <ididitrizz@gmail.com>
2026-01-30 15:20:19 -0500

    Create blackjackcode.py
    yep

---
+++ blackjackcode.py
@@ -0,0 +1,13 @@
+#Blackjack Code
+import random
+Deck=["A","A","A","A","2","2","2","2","3","3","3","3","4","4","4","4","5","5","5","5","6","6","6","6","7","7","7","7","8","8","8","8","9","9","9","9","10","10","10","10","J","J","J","J","Q","Q","Q","Q","K","K","K","K"]
+Deck=random.shuffle(Deck)
+#Deal
+PHand=[]
+DHand=[]
+PHand.append(Deck.pop[0]))
+PHand.append(Deck.pop[0]))
+DHand.append(Deck.pop[0]))
+DHand.append(Deck.pop[0]))
+print(PHand)
+print(DHand)
